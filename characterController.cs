5cb3a2f8bea102ed26a29a9b2ff6ee2556ab1c34
Efe Oran <efetalhaoran@gmail.com>
2023-01-30 20:28:52 +0300

    Odev1

---
+++ characterController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    public float speed = 1.0F;
+    private Rigidbody2D r2d;
+    private Animator _animator;
+    private Vector3 charPos;
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] GameObject _camera;
+    
+    
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>(); //caching spriterenderer
+        r2d = GetComponent<Rigidbody2D>(); //caching r2d
+        _animator = GetComponent<Animator>(); //caching anim
+        charPos = transform.position;
+       
+    }
+
+    void FixedUpdate()
+    {
+        
+        // r2d.velocity = new Vector2(speed, 0f);
+    
+        
+    }
+
+     void Update()
+    {
+        charPos = new Vector3(charPos.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime), charPos.y);
+        transform.position = charPos;
+        
+        if (Input.GetAxis("Horizontal") == 0.0f)
+        {
+            _animator.SetFloat("speed", 0.0f);
+        }
+        else
+        {
+            _animator.SetFloat("speed", 1.0f);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.01f)
+        {
+            _spriteRenderer.flipX = false;
+        } else if(Input.GetAxis("Horizontal")< -0.0f)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        
+
+    }
+
+    private void LateUpdate()
+    {
+        _camera.transform.position = new Vector3(charPos.x,charPos.y, charPos.z - 1.0f);
+ 
+
+    }
+}   
